a3f16f98acf86e3b97e6ce091c414c198e6e71a0
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-05-16 10:20:07 +0200

    Changed Font and Title Text

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,12 @@ import React from "react";
 const About = () => {
     return (
         <div>
-            <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
+            <h1 className="text">
+                GDSE Culture Map
             </h1>
+            <h2>                
+                An interactive map for comparing cultures in the field of Globally Distributed Software Engineering
+            </h2>
         </div>
     );
 };
