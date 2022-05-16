e7071a8fc2799433ad3aad5db81a14d4cab6e847
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-05-16 12:03:30 +0200

    Fix navigation bug

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ function App() {
     <Router>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="">GDSE Culture Map</Navbar.Brand>
+          <Navbar.Brand href="/">
+            GDSE Culture Map
+          </Navbar.Brand>
           <Nav >
-            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/map">Map</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
           </Nav>
