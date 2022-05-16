5b11080c5019ecaa3106769966ebd343e892527b
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-05-16 14:45:59 +0200

    Refactored Index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
 import './index.css'
 
-ReactDOM.render(
+import {createRoot} from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
