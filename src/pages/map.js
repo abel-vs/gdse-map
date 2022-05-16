7f28b2c2da604016f3fd778327b3df6fb12111c4
Sara Op den Orth <sara@opdenorth.com>
2022-05-16 13:25:40 +0200

    Add basic introduction map text

--- src/pages/map.js
+++ src/pages/map.js
@@ -13,8 +13,19 @@ function Map() {
             <div className={styles.flex_row}>
                 <div className={styles.explanation}>
                     <h1>
-                        HEllO world
+                        Welcome to our map!
                     </h1>
+                    <p>
+                        By hovering over a country, you can get a short summary of the important cultural aspects of this country.
+                        \n When you want to get a more extensive overview, click on the country.
+                        That is where you can find the three categories:
+                    </p>
+                    <ul>
+                        <li>organizational structure</li>
+                        <li>meetings</li>
+                        <li>social work settings</li>
+                    </ul>
+                    <h3>Enjoy!</h3>
                 </div>
                 <div className={styles.map}>
                     <MapChart setTooltipContent={setContent} />
