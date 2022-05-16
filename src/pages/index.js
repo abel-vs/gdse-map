3b855987fb0b1594c8060f597b720103a0657ea6
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-05-16 12:03:50 +0200

    Add text to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './index.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the GDSE Culture Map</h1>
+        <div className="content">
+            <h1 className="title">Welcome to the GDSE Culture Map</h1>
+
+            <p>Welcome to the map of cultural differences regarding globally distributed software engineering! </p>
+
+            <p>
+            We have compiled a summary of distinct differences between cultures, collected in a map such that users can quickly go through important cultural differences between them and their team members.
+            We used three separate categories: organisational structure, meetings, and social work settings.
+            </p>
+            <br/>
+            <div className='disclaimer'>
+            Disclaimer: The list of differences is not exhaustive, and the information is taken from various information resources that are mentioned on the web pages.
+            </div>
         </div>
     );
 };
