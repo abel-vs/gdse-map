217eaf1d97b0ec8a151b3e5b8425067817cf1293
nienkenijkamp <n.nijkamp@student.tudelft.nl>
2022-05-29 11:43:06 +0200

    add individualism index

--- src/pages/hofstede.js
+++ src/pages/hofstede.js
@@ -16,28 +16,45 @@ function Ref() {
 
                     <h4>Power Distance Index (PDI)</h4>
                     The Power Distance dimension refers to the degree of inequality between team members and whether the inequality is accepted between them.
-                    A high Power Distance Index indicates that team members accept an unequal, hierarchical distribution of power.
-                    A low Power Distance Index indicates that power is shared, and team members and leadership are considered almost as equals.
-                    <h5>Tips for working with team members from a high Power Distance Index</h5>
+                    A high PDI indicates that team members accept an unequal, hierarchical distribution of power.
+                    A low PDI indicates that power is shared, and team members and leadership are considered almost as equals.
+                    <h5>Tips for working with team members with a high PDI score</h5>
                     <ul>
                         <li>Communicate with the team member at your level. If you want to go to another level, ask for explicit permission.</li>
                         <li>If you contact a team member from a lower level, include the boss.</li>
                         <li>When contacting a team member, first address them by their last name, unless indicated otherwise.</li>
                     </ul>
-                    <h5>Tips for working with team members with a low Power Distance Index</h5>
+                    <h5>Tips for working with team members with a low PDI score</h5>
                     <ul>
                         <li>Go directly to the source. There is no need to bother the boss when this is not indicated.</li>
                         <li>Think before involving the boss in communication between team members. This could indicate distrust.</li>
                         <li>Skipping hierarchical levels probably will not be a problem.</li>
                     </ul>
 
-                    <h4>Individualism</h4>
+                    <h4>Individualism Versus Collectivism (IDV)</h4>
+                    Individualism refers to the strength of the ties that people have to others within their team/community.
+                    A high IDV indicates a weak connection to a community. People take less responsibility for others actions and outcomes.
+                    A low IDV indicates that people are loyal to the group to which they belong, and this group will defend their interests.
+                    <h5>Tips for working with team members with a high IDV</h5>
+                    <ul>
+                        <li>Acknowledge individual accomplishments.</li>
+                        <li>There is no need to mix work life with social life too much.</li>
+                        <li>Encourage team members to express their own ideas and have a debate amongst them.</li>
+                    </ul>
+                    <h5>Tips for working with team members with a low IDV</h5>
+                    <ul>
+                        <li>Ensure harmony, even if this means you have to suppress your feelings and emotions</li>
+                        <li>Avoid giving negative feedback in public</li>
+                    </ul>
 
-                    <h4>Masculinity</h4>
+                    <h4>Masculinity Versus Femininity (MAS)</h4>
+
 
                     <h4>Uncertainty Avoidance</h4>
 
                     <h4>Long Term Orientation</h4>
+
+                    <h4>Indulgence</h4>
 
 
                 </div>
