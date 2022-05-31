d7433e4ddc9fdd0bca3fe4961762adf406feb0b7
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-05-31 11:42:02 +0200

    Refactored references page
    Deleted unnecessary component and css file

--- src/pages/ref.js
+++ src/pages/ref.js
@@ -1,13 +1,20 @@
 import React from "react";
-import CountryRefs from "../components/CountryRefs";
-import styles from "./ref.module.css";
+import list from "../components/info.json";
 
 function Ref() {
     return (
-        <div>
-            <h1 className={styles.header}>References</h1>
-            {/*<h2>About</h2>*/}
-            <CountryRefs/>
+        <div className="content">
+            <h1>References</h1>
+            {list.countries.map((country) =>
+                <div>
+                    <h2>{country.name}</h2>
+                    <ol>
+                        {country.refs.map((ref) =>
+                            <li>{ref}</li>
+                        )}
+                    </ol>
+                </div>
+            )}
         </div>
     )
 }
