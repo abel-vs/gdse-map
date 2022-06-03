74333f33777f26da833f9996840d0bb3b08707d3
abel <abel.van.steenweghen@gmail.com>
2022-06-03 19:49:17 +0200

    Home Page update
    Added some explanatory text

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,14 +14,22 @@ const Home = () => {
                         </Gradient>
                     </h1>
                     <p className='subtitle'>An interactive map for comparing cultural differences regarding globally distributed software engineering! </p>
-                    <div>
+                    <div className="info">
                         <a href='/gdse-map/map' className="button">Explore Culture Map</a>
+                        <br/>
+                        Get to know some defining characteristics about business cultures around the globe. 
+                        Learn how organisations are structured, meetings take place, and how to behave at social events.
                     </div>
-                    <div>
+                    <div className='info'>
                         <a href='/gdse-map/hofstede' className="button">Compare Cultural Dimensions</a>
+                        <br/>
+                        Compare countries based on Hofstede's Cultural Dimensions. 
+                        Use the six defining dimensions to get a clear image on how two countries differ and how they are similar.
                     </div>
-                    <div>
-                        <a href='/gdse-map/about' className="button">Learn more about GDSE</a>
+                    <div className='info'>
+                        <a href='/gdse-map/about' className="button">Learn about the challenges of GDSE </a>
+                        <br/>
+                        Learn some general tips about handling cultural differences and how to become a better communicator and team player in a distributed setting.
                     </div>
                 </Col>
                 <Col>
